2112c71ccd667aa4cb3d59d539ad69b9b691965a
Nikhi <nikhibhambra@gmail.com>
2020-12-28 13:09:14 -0500

    added bookmarklet js file

---
+++ src/bookmarklet.js
@@ -0,0 +1,39 @@
+// // This code is injected as the href value in the README file. Unminified here for reading.
+
+// javascript:void function(e){
+//     var t=function(e){
+//         document.writeln("<!DOCTYPE html>"),
+//         document.writeln("<html>"),
+//         document.writeln("<body>"),
+//         document.writeln(""),
+//         document.writeln('<p style="font-size:20px"><b>Public Bookmarklet for viewing a whois of a site. Of course this isnt as complex as the real thing, because I got all the data below from scratch.</b></p><p style="font-size:13px"><i>made by shoe%231327</i></p>'),
+//         document.writeln('<p style="font-size:20px">DOMAIN INFO:</p>'),
+//         document.writeln(""),
+//         document.writeln('{"dig":{"header":{"id":"43226","qr":"1","opcode":"Query","aa":"false","tc":"false","rd":"false","ra":"false","ad":"false","cd":"false","rcode":"NXDOMAIN","qdcount":"1","ancount":"0","nscount":"0","arcount":"0"},"answer":[],"additional":[],"authority":[],"bind":";; Security Level : UNCHECKED\n;; HEADER SECTION\n;; id = 43226\n;; qr = 1    opcode = Query    aa = false    tc = false    rd = false\n;; ra = false    ad = false    cd = false    rcode  = NXDOMAIN\n;; qdcount = 1  ancount = 0  nscount = 0  arcount = 0\n\n;; QUESTION SECTION (1  record)\n;; :fqdn.INANY\n"},"error":false}'),
+//         document.writeln('<p id="demo"></p>'),
+//         document.writeln("<script>"),
+//         document.writeln('document.getElementById("demo").innerHTML = '),
+//         document.writeln('"DOMAIN:<br>" + window.location.href;'),
+//         document.writeln("</script>"),
+//         document.writeln("<!--"),
+//         document.writeln('<script type="application/javascript">'),
+//         document.writeln("  function getIP(json) {"),
+//         document.writeln('    document.write("CLIENT IP: ", json.ip);'),
+//         document.writeln("  }"),
+//         document.writeln("</script>"),
+//         document.writeln(""),
+//         document.writeln('<script type="application/javascript" src="https://api.ipify.org%3Fformat=jsonp%26callback=getIP"></script>'),
+//         document.writeln("-->"),
+//         document.writeln("</body>"),
+//         document.writeln("</html>"),
+//         document.writeln("<p>statuses: [ <br>"),
+//         document.writeln('            "clientTransferProhibited"'),
+//         document.writeln("            <br>"),
+//         document.writeln("            ]"),
+//         document.writeln("</p>"),
+//         document.writeln('<p style="font-size:20px">CLIENT INFO:</p>'),
+//         document.writeln('<pre id="response"></pre>'),document.writeln(""),e.get("https://api.ipdata.co/%3Fapi-key=test",function(t){e("%23response").html(JSON.stringify(t,null,4))},"jsonp"),document.writeln("</body>"),document.writeln("</html>")},n=e%26%26e.fn%26%26parseFloat(e.fn.jquery)>=1.7;if(n)t(e);else{var o=document.createElement("script");o.src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js",o.onload=o.onreadystatechange=function(){var e=this.readyState;e%26%26"loaded"!==e%26%26"complete"!==e||t(jQuery.noConflict())}}document.getElementsByTagName("head")[0].appendChild(o)}(window.jQuery);
+
+function helloWorld(){
+    alert('Hello World!')  
+}
