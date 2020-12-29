e429cba9b9255fe9c8a5775a69826cfc8bccbdd2
Nikhi <nikhibhambra@gmail.com>
2020-12-28 23:51:27 -0500

    adds bottom padding betweek semester blocks

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -14,7 +14,6 @@ if (window.location.href == 'https://acorn.utoronto.ca/sws/#/history/academic') 
         bt = document.getElementById('acorn-footer');
         n.remove(); m.remove(); bt.remove();
         t = document.getElementsByTagName('h1');
-        
         name = v[1].innerHTML.match(/<strong[^>]*>([\s\S]*?)<\/strong>/)[1];
         t[0].innerHTML = name;
         d[0].innerHTML = f[0].innerHTML;
@@ -24,6 +23,10 @@ if (window.location.href == 'https://acorn.utoronto.ca/sws/#/history/academic') 
         x.style.paddingTop = '0px';
         x.style.display = 'flex';
         x.style.justifyContent = 'center';
+        k = document.getElementsByClassName('credit-earned-section');
+        for (var j in k) {
+            j.style.marginBottom = "15px";
+        }
     } else {
         alert('See where it says Complete Academic History next to your name? Click that and try again.');
     }
@@ -52,4 +55,4 @@ if (window.location.href == 'https://acorn.utoronto.ca/sws/#/history/academic') 
 }
 
 
-javascript:(function(){})()
+javascript:(function(){if(window.location.href=='https://acorn.utoronto.ca/sws/#/history/academic'){v=document.getElementsByClassName('hidden-print');if(v.length>0){n=document.getElementById('acorn-nav-top');m=document.getElementById('acorn-nav-side-parent');d=document.getElementsByClassName('academic-history');f=document.getElementsByClassName("academic-history-report");bt=document.getElementById('acorn-footer');n.remove();m.remove();bt.remove();t=document.getElementsByTagName('h1');name=v[1].innerHTML.match(/<strong[^>]*>([\s\S]*?)<\/strong>/)[1];t[0].innerHTML=name;d[0].innerHTML=f[0].innerHTML;document.getElementById('main-content').style.maxWidth='850px';document.getElementById('main-content').style.padding='40px';x=document.getElementById('body-container');x.style.paddingTop='0px';x.style.display='flex';x.style.justifyContent='center';k=document.getElementsByClassName('credit-earned-section');for(var j in k){j.style.marginBottom="15px"}}else{alert('See where it says Complete Academic History next to your name? Click that and try again.')}}else{alert('This bookmarklet only works on Acorn! Please log into Acorn, navigate to Academic History, and try again.')}})()
